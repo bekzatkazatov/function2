4b16635a2cec9105a22fc27ebe749e0805bd894d
bekzatkazatov <bekzatx2x@gmail.com>
2023-03-10 23:58:21 +0600

    test

---
+++ script.js
@@ -0,0 +1,42 @@
+// function checkUrl(url) {
+//     if (url.length && url.includes('.') || url.includes('com') || url.includes('ru')) {
+//         return 'Correct url'
+//     } else {
+//         return 'Error'
+//     }
+// }
+// console.log(checkUrl("545324.com"))
+
+// function checkWord(word) {
+//     if (word.length >= 4 && word.length <= 8 && (word.length % 2 == 0)) {
+//         return 'Correct'
+//     } else {
+//         return 'In correct'
+//     }
+// }
+// console.log(checkWord ('gfgggd'))
+
+
+// function checkWord(word) {
+//     return [...word].map((letter, index) => {
+//         if (index % 2 == 0) {
+//             return letter
+//         } else {
+//             return 'E'
+//         }
+//     })
+// }
+//
+// console.log(checkWord('bekzat'))
+
+//
+// function checkWord(word1, word2) {
+//     if(word1[0] === " " || word2[0] === " " || word1 !== word2) {
+//         return 'In Correct'
+//     } else {
+//         return 'Correct'
+//     }
+// }
+// console.log(checkWord("medet", "medet"))
+
+
